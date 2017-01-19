e19ecae5110241d37bc0e52c03b3e60ac29ce861
“yluchanan <398778994@qq.com>
2017-01-19 11:04:34 +0800

    删除ele 无限滚动

--- src/assets/js/base.js
+++ src/assets/js/base.js
@@ -8,8 +8,6 @@ import VueBus from 'vue-bus'
 Vue.use(VueLazyload)
 Vue.use(vueResource)
 Vue.use(VueBus)
-var infiniteScroll = require('vue-infinite-scroll')
-Vue.use(infiniteScroll)
 // 导入全局css,scss路径在webpack.base.confi.js配置了别名
 require('scss/common.scss')
 module.exports = {
