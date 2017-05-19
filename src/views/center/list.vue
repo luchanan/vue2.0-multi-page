bb0e6c9a6ea3953d259a8dc8f665fd93094515f4
“luchanan <398778994@qq.com>
2017-05-19 21:39:03 +0800

    'save'

---
+++ src/views/center/list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="list">
+    i am list
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'list'
+}
+</script>
+
+<style>
+#list {
+    display: flex;
+}
+</style>
---
+++ src/views/center/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Add from './add'
+import List from './list'
+Vue.use(VueRouter)
+// 创建一个路由器实例
+// 并且配置路由规则
+const router = new VueRouter({
+  mode: 'history',
+  // base: baseUrl,
+  routes: [
+    {
+      path: '/add',
+      component: Add
+    },
+    {
+      path: '/list',
+      component: List
+    }
+  ]
+})
+// 路由器会创建一个 App 实例，并且挂载到选择符 #center 匹配的元素上。
+/* eslint-disable no-new */
+new Vue({
+  router: router
+}).$mount('#center')
